30f11aa8d1b32954237fc0a112a3247a4a5e345e
Ajay <ajayjadhav85200@gmail.com>
2025-07-22 17:58:01 +0530

    ttdevassyjewellers html page design

---
+++ script.js
@@ -0,0 +1,135 @@
+   const menuToggle = document.getElementById("menu-toggle");
+    const drawer = document.getElementById("drawer");
+    const closeDrawer = document.getElementById("close-drawer");
+    const overlay = document.getElementById("overlay");
+
+    menuToggle.addEventListener("click", () => {
+      drawer.classList.remove("-translate-x-full");
+      overlay.classList.remove("hidden");
+    });
+
+    closeDrawer.addEventListener("click", () => {
+      drawer.classList.add("-translate-x-full");
+      overlay.classList.add("hidden");
+    });
+
+    overlay.addEventListener("click", () => {
+      drawer.classList.add("-translate-x-full");
+      overlay.classList.add("hidden");
+    });
+
+
+// drawers menu
+function navigateToScheme() {
+  Toaster.postMessage("navigateScheme");
+}
+
+function navigateToBankDetails() {
+  Toaster.postMessage("navigateBankDetails");
+}
+
+function navigateToKyc() {
+  Toaster.postMessage("navigateKyc");
+}
+
+function navigateToRate() {
+  Toaster.postMessage("navigateRate");
+}
+
+function navigateSchemeRecords() {
+  Toaster.postMessage("navigateSchemeRecords");
+}
+
+function navigateToCustomerCard() {
+  Toaster.postMessage("navigateCustomerCard");
+}
+
+function navigateTransactionHistory() {
+  Toaster.postMessage("navigateTransactionHistory");
+}
+
+function navigateToMyOrder() {
+  Toaster.postMessage("navigateToMyOrder");
+}
+
+function navigateToUserProfile() {
+  Toaster.postMessage("navigateToUserProfile");
+}
+
+function navigateToDigitalGold() {
+  Toaster.postMessage("navigateToDigitalGold");
+}
+
+function navigateToPanchang() {
+  Toaster.postMessage("navigateToPanchang");
+}
+
+function navigateToFeedback() {
+  Toaster.postMessage("navigateToFeedback");
+}
+
+function navigateToSetting() {
+  Toaster.postMessage("navigateToSetting");
+}
+
+function setUserName(username) {
+  // Find the element by ID and set the text content
+  document.getElementById("user-name-display").innerText =
+    "Welcome, " + username;
+}
+function navigateToHomePage() {
+  Toaster.postMessage("navigateToHomePage");
+}
+
+
+// BrandingSlider 
+
+ const swiper = new Swiper(".BrandingSwiper", {
+      loop: true,
+      effect: "cube",
+      grabCursor: true,
+      cubeEffect: {
+        shadow: false,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    document.getElementById('prevBtn').addEventListener('click', () => swiper.slidePrev());
+    document.getElementById('nextBtn').addEventListener('click', () => swiper.slideNext());
+
+
+    
+  // services 
+
+   AOS.init({
+    duration: 800,
+    once: false,
+    disable: false, // 👈 Add this line
+  });
+  
+
+  let expanded = false;
+  function toggleBoxes() {
+    const container = document.getElementById("boxContainer");
+    const arrow = document.getElementById("arrowIcon");
+
+    if (!expanded) {
+      container.classList.remove("max-h-[6.5rem]");
+      arrow.classList.add("rotate-180");
+      expanded = true;
+    } else {
+      container.classList.add("max-h-[6.5rem]");
+      arrow.classList.remove("rotate-180");
+      expanded = false;
+    }
+  }
